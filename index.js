14475cb782cf4cb7bd4f4c4d29cc97b535577726
candescent-flow-1570 <f.iqbal.w@gmail.com>
2018-11-02 17:05:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name) {
-  return push.kittens(name)
+  push.kittens(name)
+  return kittens
 }
 function destructivelyPrependKitten(name) {
   return unshift.kittens(name)
