1b60aa9cee079bdd4a51b40ad5820f9d87f1823f
candescent-flow-1570 <f.iqbal.w@gmail.com>
2018-11-02 16:50:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
-appendKitten("kitten")
+function destructivelyAppendKitten(name) {
+  return appendKitten(name)
+}
