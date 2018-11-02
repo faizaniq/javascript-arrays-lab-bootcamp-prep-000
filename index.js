b2b15d3a58113e35133774c9e60afa2d87afc8c7
candescent-flow-1570 <f.iqbal.w@gmail.com>
2018-11-02 17:52:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function prependKitten(name) {
   return kittens.concat(name)
 }
 function removeLastKitten() {
-  return kittens.slice(-1)
+  return kittens.slice(0, 2)
 }
 function removeFirstKitten() {
   return kittens.slice(1)
